2654338258951f5846e2d5f84804fc76ac070c17
lintarrezha <lintarrezha7@gmail.com>
2023-03-20 19:36:58 +0700

    Initial commit

---
+++ src/latihanModul2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class latihanModul2 {
+
+    public static void main(String[] args) {
+        // Membuat objek Hewan dan menambahkan beberapa elemen
+        List<String> Hewan = new ArrayList<>();
+        Hewan.add("Sapi");
+        Hewan.add("Kelinci");
+        Hewan.add("Kambing");
+        Hewan.add("Unta");
+        Hewan.add("Domba");
+
+        // Membuat objek HapusHewan dan menambahkan beberapa elemen
+        List<String> HapusHewan = new ArrayList<>();
+        HapusHewan.add("Kelinci");
+        HapusHewan.add("Kambing");
+        HapusHewan.add("Unta");
+
+        // Menghapus data pada objek Hewan yang sama dengan data warna pada objek DeleteHewan
+        Iterator<String> iterator = Hewan.iterator();
+        while (iterator.hasNext()) {
+            String hewan = iterator.next();
+            if (HapusHewan.contains(hewan)) {
+                iterator.remove();
+
+                // Menampilkan elemen pada objek Hewan setelah data dihapus
+                System.out.println("Isi objek Hewan setelah penghapusan data: " + Hewan);
+            }
+        }
+    }
+}
